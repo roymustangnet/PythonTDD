0940e07a5900dac2af75fe5e98c0840a95611c17
roy mustang <roy.mustang.net@gmail.com>
2020-07-14 03:16:39 +0800

    completed functional tests for new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,7 +6,9 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.common.exceptions import WebDriverException
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -16,10 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 伊迪斯听说有一个很酷的在线待办事项应用
@@ -45,8 +55,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1： Buy peacock feather”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中有显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”
@@ -54,11 +63,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
 
         # 页面再次更新，清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # 伊迪斯想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
@@ -66,6 +74,50 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.fail('Finish the test')
 
+    def test_multiple_users_can_start_lists_at_different__urls(self):
+        # 伊迪斯新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # 她注意到清单有个唯一的URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # 现在一名叫做弗朗西斯的新用户访问了网站
+
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪斯的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪斯的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新的待办事项，新建一个清单
+        # 他不像伊迪斯那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊迪斯的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人都很满意，然后去睡觉了
 
 
 # if __name__ == '__main__':
